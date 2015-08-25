b612119df18ce31b2badf38b42cfea6d6e7f18c3
Joshua T Kalis <kalisjoshua@gmail.com>
2015-08-25 09:45:20 -0400

    cover all of the things

--- test/cli-reporter.js
+++ test/cli-reporter.js
@@ -1,21 +1,34 @@
 var assert = require('assert'),
 
-    reporter = require('../src/cli-reporter.js');
+    reporter = require('../src/cli-reporter.js'),
+
+    W_HINT,
+    WO_HINT;
+
+W_HINT = entry();
+WO_HINT = entry('error', 'section', {id: 'fake_id'}, 'message', 'locale');
 
 function entry(level, section, rule, message, locale) {
+  var result;
+
   rule = rule || ({
     hint: 'testing',
     id: 'fake_id'
   });
 
-  return {
-      code: rule.id,
-      //hint: rule.hint,
-      level: level || 'error',
-      message: message || 'message',
-      rule: locale || 'locale',
-      section: section || 'section'
-    };
+  result = {
+    code: rule.id,
+    level: level || 'error',
+    message: message || 'message',
+    rule: locale || 'locale',
+    section: section || 'section'
+  };
+
+  if (rule.hint) {
+    result.hint = rule.hint;
+  }
+
+  return result;
 }
 
 function reader(mock) {
@@ -56,10 +69,19 @@ describe('CLI - reporter', function () {
     assert.equal(2, output.length);
   });
 
-  it('should report an error', function () {
+  it('should report an error (with hint)', function () {
     var output = [];
 
-    reporter(output.push.bind(output), 'error', reader([entry()]));
+    reporter(output.push.bind(output), 'error', reader([W_HINT]));
+
+    assert.equal('\n\u001b[91merror\u001b[39m locale\n  \u001b[37mmessage\u001b[39m\u001b[90m [fake_id]\u001b[39m\u001b[96m\nHINT:\n\u001b[39mtesting', output[0]);
+    assert.equal(1, output.length);
+  });
+
+  it('should report an error (without hint)', function () {
+    var output = [];
+
+    reporter(output.push.bind(output), 'error', reader([WO_HINT]));
 
     assert.equal('\n\u001b[91merror\u001b[39m locale\n  \u001b[37mmessage\u001b[39m\u001b[90m [fake_id]\u001b[39m', output[0]);
     assert.equal(1, output.length);
